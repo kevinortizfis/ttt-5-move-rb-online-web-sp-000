61b18225cb5fb59e5ab1a63e87d3801e3947d568
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-03 20:11:02 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,7 +5,6 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-user_input = gets.strip
 def input_to_index(user_input)
   puts "#{user_input}"
 end
