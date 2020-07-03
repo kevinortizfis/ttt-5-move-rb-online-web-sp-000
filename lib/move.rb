bd0db1f3b3f41cec6f98aaa0b575c3fe53efe560
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-03 20:08:45 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(user_input)
+  puts "#{user_input}"
+end
+def converted_input
+  input_to_index(user_input).to_i
+end
+move = converted_input
